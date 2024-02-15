21e852cbc58190f88c8747e7e1608777113e9f2b
Amey Satwe <amey10051999@gmail.com>
2024-02-15 14:53:49 -0500

    Add variables to tfvars file

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # tf-gcp-infra
+
+
+# API's enabled for IAC
+- Google Compute Engine
+
+# Terraform commands used.
+This command initializes terraform and downloads the required binaries for the providers.
+- ```terraform init```
+
+To check if the config is valid we run the following command
+- ```terraform validate```
+
+To review the changes being made by our terraform config we run the following.
+- ```terraform plan```
+
+To apply changes done locally to GCP/AWS we run the following command
+- ```terraform apply```
+  
+This also runs terraform plan and requires approval before creating/modifying any resourcers
+
--- main.tf
+++ main.tf
@@ -1,26 +1,26 @@
 resource "google_compute_network" "cloud_demo_vpc" {
-  name                            = "cloud-demo-vpc"
+  name                            = var.vpc_name
   auto_create_subnetworks         = false
   routing_mode                    = "REGIONAL"
   delete_default_routes_on_create = true
 }
 
 resource "google_compute_subnetwork" "webapp" {
-  name          = "webapp"
+  name          = var.webapp_subnet_name
   ip_cidr_range = "10.0.1.0/24"
-  region        = "us-east1"
+  region        = var.region
   network       = google_compute_network.cloud_demo_vpc.name
 }
 
 resource "google_compute_subnetwork" "db" {
-  name          = "db"
+  name          = var.db_subnet_name
   ip_cidr_range = "10.0.2.0/24"
-  region        = "us-east1"
+  region        = var.region
   network       = google_compute_network.cloud_demo_vpc.name
 }
 
 resource "google_compute_route" "webapp_route" {
-  name             = "webapp-route"
+  name             = var.webapp_route_name
   network          = google_compute_network.cloud_demo_vpc.name
   dest_range       = "0.0.0.0/0"
   next_hop_gateway = "default-internet-gateway"
