b719778df802a331512c99ce815e00b297b6a3c9
Amey Satwe <amey10051999@gmail.com>
2024-02-28 16:40:31 -0500

    Terraform for assignment 5

--- compute.tf
+++ compute.tf
@@ -18,5 +18,18 @@ resource "google_compute_instance" "default" {
       network_tier = var.network_tier
     }
   }
-  depends_on = [google_compute_network.cloud_demo_vpc, google_compute_subnetwork.webapp]
+
+
+  metadata_startup_script = <<-EOT
+  #!/bin/bash
+  file=/opt/webapp/.env
+  if [ ! -f $file ]; then
+    sudo touch $file
+    sudo echo "DATABASE=${google_sql_database.main.name}" >> $file
+    sudo echo "UNAME=${google_sql_user.db_user.name}" >> $file
+    sudo echo "PASSWORD=${random_password.DB_Password.result}" >> $file
+    sudo echo "HOST=${google_sql_database_instance.main_primary.private_ip_address}" >> $file
+  fi
+  EOT
+  depends_on              = [google_compute_network.cloud_demo_vpc, google_compute_subnetwork.webapp]
 }
---
+++ db.tf
@@ -0,0 +1,66 @@
+
+resource "google_sql_database_instance" "main_primary" {
+  name             = random_string.DB_INSTANCE_NAME.id
+  database_version = "MYSQL_8_0"
+  depends_on       = [google_service_networking_connection.private_vpc_connection, google_compute_network.cloud_demo_vpc]
+  region           = var.region
+  settings {
+    tier                        = "db-f1-micro"
+    availability_type           = "REGIONAL"
+    disk_size                   = 100
+    disk_type                   = "pd-ssd"
+    deletion_protection_enabled = false
+    ip_configuration {
+      ipv4_enabled                                  = false
+      private_network                               = google_compute_network.cloud_demo_vpc.id
+      enable_private_path_for_google_cloud_services = true
+    }
+    backup_configuration {
+      enabled            = true
+      binary_log_enabled = true
+    }
+  }
+  deletion_protection = false
+}
+
+resource "google_sql_database" "main" {
+  name     = var.database_name
+  instance = google_sql_database_instance.main_primary.name
+}
+
+resource "google_sql_user" "db_user" {
+  name     = var.sql_user
+  instance = google_sql_database_instance.main_primary.name
+  password = random_password.DB_Password.result
+}
+
+resource "random_password" "DB_Password" {
+  length  = 16
+  special = true
+}
+
+resource "random_string" "DB_INSTANCE_NAME" {
+  length  = 8
+  special = false
+  lower   = true
+  upper   = false
+  numeric = false
+}
+
+
+resource "google_compute_global_address" "private_ip_block" {
+  name          = "private-ip-block"
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  ip_version    = "IPV4"
+  prefix_length = 24
+  network       = google_compute_network.cloud_demo_vpc.name
+}
+
+resource "google_service_networking_connection" "private_vpc_connection" {
+  network                 = google_compute_network.cloud_demo_vpc.name
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.private_ip_block.name]
+  depends_on              = [google_compute_global_address.private_ip_block]
+  deletion_policy         = "ABANDON"
+}
