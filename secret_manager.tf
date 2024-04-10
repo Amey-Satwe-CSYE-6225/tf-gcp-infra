bf7cea6eb0384e855be45403faac37fc239f912a
Amey Satwe <amey10051999@gmail.com>
2024-04-10 15:48:14 -0400

    secret manager now has kms

--- secret_manager.tf
+++ secret_manager.tf
@@ -1,10 +1,14 @@
-data "google_project" "project" {}
+resource "google_kms_crypto_key_iam_member" "kms-secret-binding" {
+  crypto_key_id = google_kms_crypto_key.secret_manager_key.id
+  role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
+  member        = "serviceAccount:${google_project_service_identity.gcp_sa_cloud_secret_manager.email}"
+}
 
-# resource "google_kms_crypto_key_iam_member" "kms-secret-binding" {
-#   crypto_key_id = google_kms_crypto_key.secret_manager_key.id
-#   role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
-#   member        = "serviceAccount:service-${data.google_project.project.number}@gcp-sa-secretmanager.iam.gserviceaccount.com"
-# }
+
+resource "google_project_service_identity" "gcp_sa_cloud_secret_manager" {
+  provider = google-beta
+  service  = "secretmanager.googleapis.com"
+}
 
 resource "google_secret_manager_secret" "secret-with-automatic-cmek" {
   secret_id = "webapp_secret"
@@ -13,14 +17,14 @@ resource "google_secret_manager_secret" "secret-with-automatic-cmek" {
     user_managed {
       replicas {
         location = var.region
-        # customer_managed_encryption {
-        #   kms_key_name = google_kms_crypto_key.secret_manager_key.id
-        # }
+        customer_managed_encryption {
+          kms_key_name = google_kms_crypto_key.secret_manager_key.id
+        }
       }
     }
   }
 
-  #   depends_on = [google_kms_crypto_key_iam_member.kms-secret-binding]
+  depends_on = [google_kms_crypto_key_iam_member.kms-secret-binding]
 }
 
 resource "google_secret_manager_secret_version" "webapp_secret_version" {
